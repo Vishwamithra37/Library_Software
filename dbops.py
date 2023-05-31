d3dc73cc777d2ba548bae6096b4f30ed6c6039ee
Vishwa Mithra <mithravishwa37@gmail.com>
2023-05-31 23:08:16 +0530

    Vishwa Tasks

---
+++ dbops.py
@@ -0,0 +1,69 @@
+import config
+import datetime
+from bson.objectid import ObjectId
+import easycrypt
+import json
+import copy
+import time
+
+
+
+dab = config.DB[config.DATABASE]
+
+
+def enco(sstr: str):
+    key2 = easycrypt.genkeypassword(
+        config.SESSION_ENCRYPTING_KEY, config.DBOPSENCODINGSTRING.encode('utf-8'))
+    fkey = easycrypt.encrypt((sstr).encode('utf-8'), key2)
+    fkey = fkey.decode('utf-8')
+    # print(key)
+    return fkey
+
+
+def deco(secode: str):
+    secode = secode.encode('utf-8')
+    key2 = easycrypt.genkeypassword(
+        config.SESSION_ENCRYPTING_KEY, config.DBOPSENCODINGSTRING.encode('utf-8'))
+    fkey3 = easycrypt.decrypt(secode, key2)
+    # print(fkey3)
+    return fkey3
+
+
+class inserts:
+    def register_new_user(user_Object: dict):
+        dac = dab["users"]
+        v1 = dac.insert_one(user_Object)
+        if v1.acknowledged:
+            return True
+        return False
+    
+    def create_session_token(user_Object: dict):
+        """Returns a string of encrypted session token
+        
+        Keyword arguments:
+        user_Object -- a dictionary containing the user_id
+        Returns:
+        False -- if the token could not be created (Boolean)
+        token -- if the token was created successfully (String and Encrypted)
+        """
+        
+        dac = dab["sessions"]
+        v1 = dac.insert_one(user_Object)
+        if v1.acknowledged:
+            return enco(str(v1.inserted_id))
+        return False
+    
+class getters:
+    def get_user_by_credentials(user_Object: dict):
+        dac = dab["users"]
+        v1 = dac.find_one(user_Object)
+        if v1:
+            return v1
+        return False
+    
+    def get_session_by_token(token: str):
+        dac = dab["sessions"]
+        v1 = dac.find_one({"_id":ObjectId(deco(token))})
+        if v1:
+            return v1
+        return False
--- main.py
+++ main.py
@@ -1,11 +1,58 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import flask
+import dbops
+import config
+app = flask.Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return flask.render_template('home.html')
 
+@app.route('/api/v1/user/register', methods=['POST'])
+def register():
+    Flask_JSON = flask.request.get_json()
+    ################### Validation ###################
+    expected_keys = ['username', 'password', 'email']
+    if list(set(expected_keys) - set(Flask_JSON.keys())) != []:
+        return {'status': 'error', 'message': 'Missing keys'}, 400
+    if 20<len(Flask_JSON['username']) < 2: return {'status': 'error', 'message': 'Username too short'}, 400
+    if 50<len(Flask_JSON['password']) < 2: return {'status': 'error', 'message': 'Password too short'}, 400
+    if 50<len(Flask_JSON['email']) < 2: return {'status': 'error', 'message': 'Email too short'}, 400
+    if not '@' in Flask_JSON['email']: return {'status': 'error', 'message': 'Invalid email'}, 400
+    if not '.' in Flask_JSON['email']: return {'status': 'error', 'message': 'Invalid email'}, 400
+    ################## End Validation #################
+    Flask_JSON["Role"]="Student"
+    Flask_JSON["password"]=dbops.enco(Flask_JSON["password"])
+    Flask_JSON["permissions"]=[]
+    step1= dbops.inserts.register_new_user(Flask_JSON)
+    if step1:
+        return {'status': 'success'}, 200
+    return {'status': 'error', 'message': 'Username already exists'}, 400
+
+@app.route('/api/v1/user/login', methods=['POST'])
+def login():
+    Flask_JSON = flask.request.get_json()
+    ################### Validation ###################
+    expected_keys = ['email', 'password']
+    if list(set(expected_keys) - set(Flask_JSON.keys())) != []:
+        return {'status': 'error', 'message': 'Missing keys'}, 400
+    if 50<len(Flask_JSON['password']) < 2: return {'status': 'error', 'message': 'Password too short'}, 400
+    if 50<len(Flask_JSON['email']) < 2: return {'status': 'error', 'message': 'Email too short'}, 400
+    if not '@' in Flask_JSON['email']: return {'status': 'error', 'message': 'Invalid email'}, 400
+    if not '.' in Flask_JSON['email']: return {'status': 'error', 'message': 'Invalid email'}, 400
+    ################## End Validation #################
+    Flask_JSON["password"]=dbops.enco(Flask_JSON["password"])
+    step1= dbops.getters.get_user_by_credentials(Flask_JSON)
+    if step1:
+        del step1["_id"]
+        step2=dbops.inserts.create_session_token(step1)
+        if step2:
+            r1=dbops.enco(str(step1["_id"]))
+            flask.session["Top_Secret_Token"] = r1
+            flask.session.permanent = True
+            return {'status': 'success', 'token': step2}, 200
+        return {'status': 'error', 'message': 'Internal error'}, 500
+    return {'status': 'error', 'message': 'Invalid credentials'}, 400
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8000, debug=True)
+    app.run(host='localhost', port=80, debug=True)
