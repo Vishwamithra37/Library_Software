85b234f8e448386ab1e27fb8b6b41052c7dba317
Vishwa Mithra <mithravishwa37@gmail.com>
2023-06-02 21:38:29 +0530

    Tests update

--- backend/Tests/API_TEST_KIT.py
+++ backend/Tests/API_TEST_KIT.py
@@ -7,9 +7,10 @@ TEST_ARRAY=[]
 
 # This is the test kit. It is used to test the various api endpoints with the help of the requests library.
 
-# Testing registration endpoint.
+
 # This is the test kit. It is used to test the various api endpoints with the help of the requests library.
 
+# Testing registration endpoint.
 def test_register():
         """This function tests the registration endpoint.
         It sends a post request to the endpoint with the following data:
@@ -45,7 +46,6 @@ def test_register():
             raise Exception("Registration Failed")
         return r.json()
             
-
 # Testing login endpoint.
 def test_login():
         """This function tests the login endpoint.
@@ -81,6 +81,9 @@ def test_login():
             raise Exception("Login Failed")
         return r.json()
 
+# Testing book registration endpoint.
+def test_book_registration():
+     pass
 
 
 # This class is used to delete stuff from the database after testing.
@@ -90,20 +93,36 @@ class deleters:
         dac.delete_one({"email": "tester@gmail.com"})
 
      def delete_test_sessions():
-          dac=dab["SESSIONS"]
-          dac.delete_one({"email": "tester@gmail.com"}) 
+        dac=dab["SESSIONS"]
+        dac.delete_one({"email": "tester@gmail.com"}) 
 
+class verifiers:
+     def verify_test_user_registration():
+          dac=dab["USERS"]
+          v1=dac.find_one({"email": "tester@gmail.com"})
+          if v1:
+               return True
+          return False
+     def verify_test_user_login():
+          dac=dab["SESSIONS"]
+          v1=dac.find_one({"email": "tester@gmail.com"})
+          if v1:
+                return True
+          return False
 
 # Testing registration endpoint.
 class TestKits:
     def registration_kit():
         print("Registration Kit")
         test_register()
+        if not verifiers.verify_test_user_registration(): raise Exception("Registration Failed")
         deleters.delete_test_users()
     def registration_and_login_kit():
         print("Registration and Login Kit")
         test_register()
         test_login()
+        if not verifiers.verify_test_user_registration(): raise Exception("Registration Failed")
+        if not verifiers.verify_test_user_login(): raise Exception("Login Failed")
         deleters.delete_test_users()
         deleters.delete_test_sessions()
     
